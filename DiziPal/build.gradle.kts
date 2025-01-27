0aa0296a0d9fd01970dc8e15f65b73a5c0e702e4
sarapcanagii <kral42613@gmail.com>
2025-01-27 05:26:02 +0300

    Update build.gradle.kts

--- DiziPal/build.gradle.kts
+++ DiziPal/build.gradle.kts
@@ -2,16 +2,17 @@ version = 22
 
 cloudstream {
     authors     = listOf("sarapcanagii")
-        language    = "tr"
-            description = "en yeni dizileri güvenli ve hızlı şekilde sunar."
+    language    = "tr"
+    description = "en yeni dizileri güvenli ve hızlı şekilde sunar."
 
-                /**
-                     * Status int as the following:
-                          * 0: Down
-                               * 1: Ok
-                                    * 2: Slow
-                                         * 3: Beta only
-                                             **/
-                                                 status  = 1 // will be 3 if unspecified
-                                                     tvTypes = listOf("TvSeries", "Movie")
-                                                         iconUrl = "https://www.google.com/s2/favicons?domain=https://dizipal683.com&sz
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1 // will be 3 if unspecified
+    tvTypes = listOf("TvSeries", "Movie")
+    iconUrl = "https://www.google.com/s2/favicons?domain=https://dizipal683.com&sz=%size%"
+}
