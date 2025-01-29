90f9a70b263a694f5e822ecf9a606a02a0f12610
sarapcanagi <kral42613@gmail.com>
2025-01-29 19:43:36 +0300

    update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,7 @@ subprojects {
 
     cloudstream {
         // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
-        setRepo(System.getenv("MY_GITHUB_REPOSITORY") ?: "https://github.com/sarapcanagii/Pitipitii")
+        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "user/repo")
 
         authors = listOf("sarapcanagii")
     }
