14647d9861d693aebd9b7b70984f96c5b7fd50e1
sarapcanagi <kral42613@gmail.com>
2025-01-29 20:30:42 +0300

    update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:7.4.2")
-        classpath("com.github.recloudstream:gradle:main-SNAPSHOT")
+        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.20")
     }
 }
