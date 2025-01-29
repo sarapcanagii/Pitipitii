1e40caadcbf32b47491d82efa39918233fd37247
sarapcanagi <kral42613@gmail.com>
2025-01-29 20:25:51 +0300

    update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,21 @@
 import com.lagradost.cloudstream3.gradle.CloudstreamExtension
 import com.android.build.gradle.BaseExtension
 
+plugins {
+    id("com.android.library") apply false
+    id("org.jetbrains.kotlin.android") version "1.8.20" apply false
+}
+
 buildscript {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
+        maven(url = "https://jitpack.io")
     }
 
     dependencies {
         classpath("com.android.tools.build:gradle:7.4.2")
-        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
+        classpath("com.github.recloudstream:gradle:main-SNAPSHOT")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.20")
     }
 }
@@ -19,25 +24,21 @@ allprojects {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
+        maven(url = "https://jitpack.io")
     }
 }
-
-fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) = extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
-
-fun Project.android(configuration: BaseExtension.() -> Unit) = extensions.getByName<BaseExtension>("android").configuration()
 
 subprojects {
     apply(plugin = "com.android.library")
     apply(plugin = "kotlin-android")
     apply(plugin = "com.lagradost.cloudstream3.gradle")
 
-    cloudstream {
+    configure<com.lagradost.cloudstream3.gradle.CloudstreamExtension> {
         setRepo(System.getenv("GITHUB_REPOSITORY") ?: "user/repo")
         authors = listOf("sarapcanagii")
     }
 
-    android {
+    configure<com.android.build.gradle.LibraryExtension> {
         namespace = "com.sarapcanagii"
 
         compileSdk = 33
@@ -52,29 +53,24 @@ subprojects {
             targetCompatibility = JavaVersion.VERSION_1_8
         }
 
-        tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-            kotlinOptions {
-                jvmTarget = "1.8"
-            }
+        kotlinOptions {
+            jvmTarget = "1.8"
         }
     }
 
     dependencies {
-        val apk by configurations
-        val implementation by configurations
+        "apk"("com.lagradost:cloudstream3:pre-release")
 
-        apk("com.lagradost:cloudstream3:pre-release")
-
-        implementation(kotlin("stdlib"))
-        implementation("com.github.Blatzar:NiceHttp:0.4.11")
-        implementation("org.jsoup:jsoup:1.18.3")
-        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.0")
-        implementation("com.fasterxml.jackson.core:jackson-databind:2.16.0")
-        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+        "implementation"(kotlin("stdlib"))
+        "implementation"("com.github.Blatzar:NiceHttp:0.4.11")
+        "implementation"("org.jsoup:jsoup:1.18.3")
+        "implementation"("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.0")
+        "implementation"("com.fasterxml.jackson.core:jackson-databind:2.16.0")
+        "implementation"("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
     }
 }
 
-tasks.register<Delete>("clean") {
+tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
 
