86186d9ec268dedc0bcc1dd77e170d46bbbed690
sarapcanagii <kral42613@gmail.com>
2025-02-27 04:01:54 +0300

    update

---
+++ TFlix/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 22
+
+cloudstream {
+    authors     = listOf("sarapcanagii")
+    language    = "tr"
+    description = "(TEST AŞAMASINDA) Watch live sports on TFLIX, including English Premier League, LaLiga, Bundesliga, F1, and live cricket. Stream your favorite events MHDTV Sports &amp; DaddyLiveHD instantly!"
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1 // will be 3 if unspecified
+    tvTypes = listOf("Live")
+    iconUrl = "https://www.google.com/s2/favicons?domain=https://tv.tflix.app&sz=%size%"
+}
