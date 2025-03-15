4ca2d39deec2e808f5905766c9672dfdbca2052a
sarapcanagii <kral42613@gmail.com>
2025-03-15 18:03:44 +0300

    update

---
+++ VavooSpor/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 4
+
+cloudstream {
+    authors     = listOf("sarapcanagii")
+    language    = "tr"
+    description = "VavooSpor"
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1 // will be 3 if unspecified
+    tvTypes = listOf("Live")
+    iconUrl = "https://www.google.com/s2/favicons?domain=www.vavoo.com&sz=%size%"
+}
