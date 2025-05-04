67e2b464c13be09a04e9db59774a8c3406108e98
sarapcanagii <kral42613@gmail.com>
2025-05-04 16:12:15 +0300

    update build.gradle.kts

--- DiziPal/build.gradle.kts
+++ DiziPal/build.gradle.kts
@@ -6,13 +6,14 @@ cloudstream {
     description = "En yeni dizileri güvenli ve hızlı şekilde sunar."
     status  = 1
     tvTypes = listOf("TvSeries", "Movie")
-    
-    val mainFile = project.file("src/main/kotlin/com/Prueba/DiziPalV2.kt")
+
+// Bu kod sarapcanagii ve primatzeka' ya aittir. İstediginiz gibi kullanabilirsiniz.
+    val mainFile = project.file("src/main/kotlin/com/Pitipitii/DiziPal.kt")
     val mainContent = mainFile.readText()
     val mainUrlRegex = """mainUrl\s*=\s*"https://([^"]+)"""".toRegex()
     
     val domain = mainUrlRegex.find(mainContent)?.groupValues?.get(1) 
-        ?: throw GradleException("Domain DiziPalV2.kt dosyasından çekilemedi")
+        ?: throw GradleException("Domain DiziPal.kt dosyasından çekilemedi")
     
     iconUrl = "https://www.google.com/s2/favicons?domain=$domain&sz=32"
 }
