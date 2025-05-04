c6809724b3eb5d41680e578705d25ec0ff8c8372
sarapcanagii <kral42613@gmail.com>
2025-05-04 16:08:18 +0300

    dinamik şekilde favicon çeken kod eklendi

--- DiziPal/build.gradle.kts
+++ DiziPal/build.gradle.kts
@@ -3,16 +3,16 @@ version = 32
 cloudstream {
     authors     = listOf("sarapcanagii")
     language    = "tr"
-    description = "en yeni dizileri güvenli ve hızlı şekilde sunar."
-
-    /**
-     * Status int as the following:
-     * 0: Down
-     * 1: Ok
-     * 2: Slow
-     * 3: Beta only
-    **/
-    status  = 1 // will be 3 if unspecified
+    description = "En yeni dizileri güvenli ve hızlı şekilde sunar."
+    status  = 1
     tvTypes = listOf("TvSeries", "Movie")
-    iconUrl = "https://www.google.com/s2/favicons?domain=https://dizipal683.com&sz=%size%"
+    
+    val mainFile = project.file("src/main/kotlin/com/Prueba/DiziPalV2.kt")
+    val mainContent = mainFile.readText()
+    val mainUrlRegex = """mainUrl\s*=\s*"https://([^"]+)"""".toRegex()
+    
+    val domain = mainUrlRegex.find(mainContent)?.groupValues?.get(1) 
+        ?: throw GradleException("Domain DiziPalV2.kt dosyasından çekilemedi")
+    
+    iconUrl = "https://www.google.com/s2/favicons?domain=$domain&sz=32"
 }
