b6520574b5d7db290c3adbd08046403a6a1d64bf
sarapcanagii <kral42613@gmail.com>
2025-08-21 14:42:02 +0300

    update

--- NeonSpor/src/main/kotlin/com/Pitipitii/NeonSpor.kt
+++ NeonSpor/src/main/kotlin/com/Pitipitii/NeonSpor.kt
@@ -8,7 +8,7 @@ import com.lagradost.cloudstream3.utils.AppUtils.toJson
 import java.io.InputStream
 
 class NeonSpor : MainAPI() {
-    override var mainUrl              = "https://raw.githubusercontent.com/sarapcanagii/Pitipitii/refs/heads/master/NeonSpor/NeonSpor.m3u8"
+    override var mainUrl              = "https://raw.githubusercontent.com/primatzeka/kurbaga/main/NeonSpor/NeonSpor.m3u"
     override var name                 = "NeonSpor"
     override val hasMainPage          = true
     override var lang                 = "tr"
